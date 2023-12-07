3a68522cc999e00edb1a58f9674e03cd7386aee9
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-07 16:38:09 +0300

    Collections

--- src/main/kotlin/list.kt
+++ src/main/kotlin/list.kt
@@ -1,4 +1,22 @@
-fun main(args: Array<String>) {
-    val numbersList: List<Any> = listOf(1, "Common", 3, 4, 5)
+fun main() {
+    val numbersList: List<Int> = listOf(1, 2, 3, 4, 5)
     println("Immutable List: $numbersList")
+
+    val uniqueNumbersSet: Set<Int> = setOf(1, 2, 3, 4, 5)
+    println("Immutable Set: $uniqueNumbersSet")
+
+    val fruitPrices: Map<String, Double> = mapOf("apple" to 1.0, "banana" to 0.75, "orange" to 1.5)
+    println("Immutable Map: $fruitPrices")
+
+    val mutableNumbersList: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
+    mutableNumbersList.add(6)
+    println("Mutable List: $mutableNumbersList")
+
+    val mutableUniqueNumbersSet: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    mutableUniqueNumbersSet.add(6)
+    println("Mutable Set: $mutableUniqueNumbersSet")
+
+    val mutableFruitPrices: MutableMap<String, Double> = mutableMapOf("apple" to 1.0, "banana" to 0.75)
+    mutableFruitPrices["orange"] = 1.5
+    println("Mutable Map: $mutableFruitPrices")
 }
