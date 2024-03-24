1caee5db46639b994d55830405200c04eb714716
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-03-24 23:34:19 +0300

    Kotlin Daily challenge

---
+++ src/main/kotlin/kotlinserver.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val server = KotlinServer()
+    server.startServer()
+}
+
+class KotlinServer {
+    fun startServer() {
+        println("Server Connected")
+    }
+
+}
---
+++ src/main/kotlin/simplefunction.kt
@@ -0,0 +1,17 @@
+fun justry() {
+    println("Let's Try Something new")
+    val a = 167
+    val b = 207
+    val c = 778
+    println(a + b + c)
+
+    val listOftowns = arrayOf("Nairobi", "Kisumu", "Eldoret", "Mombasa", "Nakuru")
+    println("List of towns:")
+    for (town in listOftowns) {
+        println(town)
+    }
+}
+
+fun main() {
+    justry()
+}
